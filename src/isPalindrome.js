9472128fd74e3a253bce0ae1902e01924cd128c4
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-11-07 21:09:52 +0200

    add regex to rm whitespace

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,11 +1,11 @@
 export function isPalindrome(input) {
-  // compare input and reversed input, return empty string if true, return "NOT" if false
-  // make it string type and uppercase for easy comparing
+  // compare input and reversed input
+  // make it string type and uppercase and remove white space for easy comparing
   if (input === "") {
     return "";
   }
   let reversed = "";
-  let string = String(input).toUpperCase();
+  let string = String(input).toUpperCase().replace(/\s/g, "");
   for (let i = string.length - 1; i >= 0; i--) {
     reversed += string[i];
     if (string === reversed) return "This is a Palindrome!";
